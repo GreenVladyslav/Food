c58cb6cd11f0e2bcef57f62906415b4551745706
Vladyslav <jrlimueno@gmail.com>
2022-09-14 10:45:15 +0300

    tabs

---
+++ js/script.js
@@ -0,0 +1,41 @@
+'use strict';
+
+window.addEventListener('DOMContentLoaded', () => {
+    const tabsContent = document.querySelectorAll('.tabcontent'),
+          tabs = document.querySelectorAll('.tabheader__item'),
+          tabsParent = document.querySelector('.tabheader__items');
+
+
+    function hideTabsContent() {
+        tabsContent.forEach(item => {
+            item.classList.add('hide');
+            item.classList.remove('show', 'fade');
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabsContent(i = 0) {
+        tabsContent[i].classList.remove('hide');
+        tabsContent[i].classList.add('show', 'fade');
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabsContent();
+    showTabsContent();
+
+    tabsParent.addEventListener('click', (event) => {
+        let target = event.target;
+
+        if (target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, index) => {
+                if(target == item) {
+                    hideTabsContent();
+                    showTabsContent(index);
+                }
+            });
+        }
+    });
+});
